dc3471cb783d1f091a8bdd032af65b2a4a6df360
Mahanvali <139391502+Mahanvali@users.noreply.github.com>
2024-07-24 17:21:25 +0330

    Add support for complex roots (imaginary numbers)

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,9 @@ package quadfunc.src;
 
 import java.util.Scanner;
 
-// # TODO: ADD SUPPORT FOR DISCRIMINANT < 0
-
 public class Main {
     public static void main(String[] args){
-        try (Scanner scannerObj = new Scanner(System.in)) { //  Using try so I don't have to close it
+        try (Scanner scannerObj = new Scanner(System.in)) { //  Using try so I don't have to close the Scanner
             while (true) {
                 System.out.println("Quadratic function formula: ax^2 + bx + c");
                 System.out.print("a = ");
@@ -26,22 +24,23 @@ public class Main {
         if(discriminant >= 0){
             double posroot = (-b + Math.sqrt(discriminant)) / 2*a;
             double negroot = (-b - Math.sqrt(discriminant)) / 2*a;
+            System.out.println("--------------------------------------------------------------------------------------");   //  Seperator
             System.out.println("Negative Root: " + negroot);
             System.out.println("Positive Root: " + posroot);
         } else if(discriminant < 0){
-            System.out.println("The discriminant is less than 0");
+            double realPart = -b / (2 * a);
+            double imaginaryPart = Math.sqrt(-discriminant) / (2 * a);
+            System.out.println("--------------------------------------------------------------------------------------");   //  Seperator
+            System.out.println("Complex Roots: " + realPart + " ± " + imaginaryPart + "i");
         }
     }
 
     static void CalculateVertices(double a, double b, double c){
         double discriminant = Math.pow(b, 2) - 4*a*c;
-        if(discriminant >= 0){
-            double xvertex = -b / 2*a;
-            double yvetex = -discriminant / 4*a;
-            System.out.println("X Vertex: " + xvertex);
-            System.out.println("Y Vertex: " + yvetex);
-        } else if(discriminant < 0){
-            System.out.println("The discriminant is less than 0");
-        }
+        double xvertex = -b / 2*a;
+        double yvetex = -discriminant / 4*a;
+        System.out.println("X Vertex: " + xvertex);
+        System.out.println("Y Vertex: " + yvetex);
+        System.out.println("--------------------------------------------------------------------------------------");   //  Seperator
     }
 }
